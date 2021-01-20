4e711621065dd2d091c6fb026ff5b8b071b85f03
adaptrix <mwakalingajohn@gmail.com>
2021-01-20 21:09:10 +0300

    - delete dist folder

--- dist/lib/parsers/BaseResponseParser.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var BaseResponseParser = /** @class */ (function () {
-    function BaseResponseParser(html) {
-        this.html = html;
-    }
-    return BaseResponseParser;
-}());
-exports.default = BaseResponseParser;
